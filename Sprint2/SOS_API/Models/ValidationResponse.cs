a963bae8555a5e70fb83c0ea9cf866cb9164aa84
Lars Yarkosky <lhyarkosky@gmail.comgit config --global user.email>
2025-10-21 12:38:27 -0500

    deleting unused file

--- Sprint2/SOS_API/Models/ValidationResponse.cs
+++
@@ -1,8 +0,0 @@
-namespace SOS_API.Models
-{
-    public class ValidationResponse
-    {
-        public bool IsValid { get; set; }
-        public string Message { get; set; } = string.Empty;
-    }
-}
